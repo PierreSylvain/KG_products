8f0a22c4483aaa5f1e43500e74e6328093241c88
pierre-sylvain.augereau <pierre-sylvain.augereau@raccourci.fr>
2024-09-19 11:09:23 +0200

    Add dataset comment and refactor for PEP8 compliance
    - Added a comment specifying the location of the original dataset (HuggingFace).
    - Refactored the code to ensure compliance with PEP8 standards, improving readability and consistency.

--- src/create_main_entities.py
+++ src/create_main_entities.py
@@ -1,3 +1,7 @@
+"""
+Data are get from the following dataset:
+https://huggingface.co/datasets/EmbeddingStudio/amazon-products-with-images
+"""
 import pandas as pd
 
 from src.modules.neo4j import Neo4j
@@ -5,12 +9,16 @@ from src.modules.parse_specifications import parse_specifications
 
 
 def load_data():
-    # Source: https://huggingface.co/datasets/EmbeddingStudio/amazon-products-with-images
-    eval_data = pd.read_parquet("../data/eval-00000-of-00001.parquet", engine='pyarrow')
+
+    eval_data = pd.read_parquet("../data/eval-00000-of-00001.parquet",
+                                engine='pyarrow')
     eval_data = eval_data.drop(columns=["Image", "Raw Image"])
     # Create a new column with specifications as a dictionary
-    eval_data['Parsed Specifications'] = eval_data['Product Specification'].apply(parse_specifications)
-    eval_data.to_json("../data/eval_data.json", orient="records", lines=True)
+    eval_data['Parsed Specifications'] = eval_data[
+        'Product Specification'].apply(parse_specifications)
+    eval_data.to_json("../data/eval_data.json",
+                      orient="records",
+                      lines=True)
     return data
 
 
