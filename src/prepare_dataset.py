3575812b39a942ad139f9f0afba1fcd17cb2aacf
pierre-sylvain <ps.augereau@gmail.com>
2024-09-21 13:34:01 +0200

    Separate Data Preparation and Neo4j Entity Creation
    Refactors the code to introduce a clear separation between dataset preparation and database operations.
    
    Added prepare_dataset.py for handling dataset preprocessing. This includes:
     - Selecting only the necessary fields.
     - Splitting or ungluing words in the description field to facilitate later operations.
    
    Modified create_main_entities.py to focus solely on managing the creation of entities and relationships in Neo4j, ensuring that this file remains dedicated to database interactions without handling data transformations.
    
    This refactor improves code readability, maintainability, and separation of concerns, allowing for easier updates and testing of each process independently.

---
+++ src/prepare_dataset.py
@@ -0,0 +1,42 @@
+"""
+Data are get from the following dataset:
+https://huggingface.co/datasets/EmbeddingStudio/amazon-products-with-images
+"""
+from concurrent.futures import ThreadPoolExecutor
+
+import polars as pl
+
+from src.modules.neo4j import Neo4j
+from src.modules.parse_specifications import parse_specifications
+
+
+def parallel_parse_specifications(specifications_list, func, max_workers=4):
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        results = list(executor.map(func, specifications_list))
+    return results
+
+
+def prepare_dataset():
+    eval_data = pl.read_parquet("../data/eval-00000-of-00001.parquet")
+
+    # Extract "Product Specification" column as a Python list
+    spec_list = eval_data["Product Specification"].to_list()
+
+    # Apply the custom parsing function in parallel
+    parsed_specs = parallel_parse_specifications(spec_list,
+                                                 parse_specifications,
+                                                 max_workers=4)
+
+    # Drop the specified columns
+    eval_data = eval_data.drop(["Image", "Raw Image"])
+
+    # Add the processed specifications back as a new column
+    eval_data = eval_data.with_columns(
+        pl.Series("Parsed Specifications", parsed_specs))
+
+    # Save the resulting DataFrame back to a parquet file
+    eval_data.write_parquet("../data/product_data.parquet")
+
+
+if __name__ == "__main__":
+    prepare_dataset()
